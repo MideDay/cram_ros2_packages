7bd5e0403841bc3c875258e2d273a67cdafc24e8
fmuehlis <fmuehlis@posteo.de>
2026-06-09 15:26:49 +0200

    fix: link main executable directly to robokudo

--- robokudo_ros/setup.py
+++ robokudo_ros/setup.py
@@ -28,7 +28,7 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            "main = robokudo_ros.scripts.main:main",
+            "main = robokudo.scripts.main:main",
         ],
     },
 )
